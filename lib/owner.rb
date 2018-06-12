0bac6bde73b87445a4136ae99585e7e3af014c60
Adam Urbina <adamurbina@gmail.com>
2018-06-12 23:28:58 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,17 @@
 class Owner
-  # code goes here
+
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def self.count
+        self.all.count
+    end
+
+    def reset_all
+        @@all = []
+    end
+
 end
