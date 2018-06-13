27ac07b5a6abf5f562dab2422ae65b3d5f4e04f3
Adam Urbina <adamurbina@gmail.com>
2018-06-13 00:07:21 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,6 +57,10 @@ class Owner
         fish.mood = "happy"}
     end
 
+    def sell_pets
+        
+    end
+
     def list_pets
         "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
     end
